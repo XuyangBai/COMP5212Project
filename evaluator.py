e1ec2fbe4cd39b9911b584281effc0bbb30695d7
hyzkent <zhuam@cse.ust.hk>
2018-11-24 11:42:44 +0800

    minor change

--- evaluator.py
+++ evaluator.py
@@ -22,19 +22,19 @@ class evaluator:
         self.recall = 0
         self.f1_score = 0
     
-    def __count_hamming_loss(self, outputs, targets):
+    def _count_hamming_loss(self, outputs, targets):
         hl_sum = 0
         for i in outputs.shape[0]:
             hl_sum += hamming_loss(targets[i], outputs[i])
         self.hamming_loss = hl_sum / outputs.shape[0]
     
-    def __cout_Jaccard_index(self, outputs, targets):
+    def _cout_Jaccard_index(self, outputs, targets):
         Ji_sum = 0
         for i in outputs.shape[0]:
             Ji_sum += jaccard_similarity_score(targets[i], outputs[i])
         self.Jaccard_index = Ji_sum / outputs.shape[0]
 
-    def __count_precision_and_recall(self, outputs, targets):
+    def _count_precision_and_recall(self, outputs, targets):
         pre_sum = 0
         rec_sum = 0
         for i in outputs.shape[0]:
@@ -44,11 +44,11 @@ class evaluator:
         self.recall = rec_sum / outputs.shape[0]
         self.precision = pre_sum / outputs.shape[0]
 
-    def __count_f1_score(self):
+    def _count_f1_score(self):
         self.f1_score = 2 * (self.precision * self.recall) / (self.precision + self.recall)
 
 
-    def __forward_pass(self):
+    def _forward_pass(self):
         ''' forward in_ through the net, return outputs '''
         ''' forward process of the trained model is defined as net_forward '''
         input_var = Variable(self.test_img).cuda(async=True)
@@ -58,10 +58,10 @@ class evaluator:
     def evaluate(self):
         ''' evaluate the net on the data in the loader '''
         outputs = self.__forward_pass()
-        self.__count_hamming_loss(outputs, self.test_label)
-        self.__cout_Jaccard_index(outputs, self.test_label)
-        self.__count_precision_and_recall(outputs, self.test_label)
-        self.__count_f1_score()
+        self._count_hamming_loss(outputs, self.test_label)
+        self._cout_Jaccard_index(outputs, self.test_label)
+        self._count_precision_and_recall(outputs, self.test_label)
+        self._count_f1_score()
     
     def get_hamming_loss(self):
         return self.hamming_loss
