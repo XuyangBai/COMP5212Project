2bd8886242bef8eeb2687710f5d3a36a62dbbddc
XuyangBai <653823597@qq.com>
2018-11-24 11:59:47 +0800

    Add files via upload

--- evaluator.py
+++ evaluator.py
@@ -4,6 +4,7 @@ from sklearn.metrics import jaccard_similarity_score
 from sklearn.metrics import confusion_matrix
 import torch
 from torch.autograd import Variable
+from dataloader import get_data_loader
 
 '''
 Helper methods for evaluating a classification network
@@ -11,69 +12,71 @@ Helper methods for evaluating a classification network
 The output of the model and test_label should be in the format of [batch_size , n_category] np.matrix
 '''
 
-class evaluator:
-    def __init__(self, trained_model, test_img, test_label):
-        self.trained_model = trained_model
-        self.test_img = test_img
-        self.test_label = test_label
-        self.hamming_loss = 0
-        self.Jaccard_index = 0
-        self.precision = 0
-        self.recall = 0
-        self.f1_score = 0
-    
-    def _count_hamming_loss(self, outputs, targets):
-        hl_sum = 0
-        for i in outputs.shape[0]:
-            hl_sum += hamming_loss(targets[i], outputs[i])
-        self.hamming_loss = hl_sum / outputs.shape[0]
-    
-    def _cout_Jaccard_index(self, outputs, targets):
-        Ji_sum = 0
-        for i in outputs.shape[0]:
-            Ji_sum += jaccard_similarity_score(targets[i], outputs[i])
-        self.Jaccard_index = Ji_sum / outputs.shape[0]
 
-    def _count_precision_and_recall(self, outputs, targets):
-        pre_sum = 0
-        rec_sum = 0
-        for i in outputs.shape[0]:
-            cm = confusion_matrix(targets[i], outputs[i])
-            rec_sum += np.diag(cm) / np.sum(cm, axis = 1)
-            pre_sum += np.diag(cm) / np.sum(cm, axis = 0)
-        self.recall = rec_sum / outputs.shape[0]
-        self.precision = pre_sum / outputs.shape[0]
-
-    def _count_f1_score(self):
-        self.f1_score = 2 * (self.precision * self.recall) / (self.precision + self.recall)
+def __count_hamming_loss(outputs, targets):
+    hl_sum = 0
+    for i in outputs.shape[0]:
+        hl_sum += hamming_loss(targets[i], outputs[i])
+    return hl_sum / outputs.shape[0]
 
 
-    def _forward_pass(self):
-        ''' forward in_ through the net, return outputs '''
-        ''' forward process of the trained model is defined as net_forward '''
-        input_var = Variable(self.test_img).cuda(async=True)
-        outputs = self.trained_model.net_forward(input_var)
-        return outputs
+def __cout_Jaccard_index(outputs, targets):
+    Ji_sum = 0
+    for i in outputs.shape[0]:
+        Ji_sum += jaccard_similarity_score(targets[i], outputs[i])
+    return Ji_sum / outputs.shape[0]
 
-    def evaluate(self):
-        ''' evaluate the net on the data in the loader '''
-        outputs = self._forward_pass()
-        self._count_hamming_loss(outputs, self.test_label)
-        self._cout_Jaccard_index(outputs, self.test_label)
-        self._count_precision_and_recall(outputs, self.test_label)
-        self._count_f1_score()
-    
-    def get_hamming_loss(self):
-        return self.hamming_loss
-    
-    def get_Jaccard_index(self):
-        return self.Jaccard_index
-    
-    def get_precision(self):
-        return self.precision
 
-    def get_recall(self):
-        return self.recall
-    
-    def get_f1_score(self):
-        return self.f1_score
+# def __count_precision_and_recall(outputs, targets):
+#     pre_sum = 0
+#     rec_sum = 0
+#     for i in outputs.shape[0]:
+#         cm = confusion_matrix(targets[i], outputs[i])
+#         rec_sum += np.diag(cm) / np.sum(cm, axis=1)
+#         pre_sum += np.diag(cm) / np.sum(cm, axis=0)
+#     recall = rec_sum / outputs.shape[0]
+#     precision = pre_sum / outputs.shape[0]
+#     return recall, precision
+#
+# def __count_f1_score(self):
+#     self.f1_score = 2 * (precision * self.recall) / (self.precision + self.recall)
+
+def __count_acc(outputs, targets):
+    # [n,1,28]
+    acc = [0 for _ in range(28)]
+    for i in range(28):
+        acc[i] = (outputs[:, :, i] == targets[:, :, i]).sum() / outputs.shape[0]
+    return acc
+
+
+def __forward_pass(model, input):
+    ''' forward in_ through the net, return outputs '''
+    ''' forward process of the trained model is defined as net_forward '''
+    input_var = Variable(input).cuda(async=True)
+    outputs = model.net_forward(input_var)
+    return outputs
+
+
+def evaluate(trained_model, batch_size=32):
+    ''' evaluate the net on the data in the loader '''
+    model = trained_model
+    loader = get_data_loader(batch_size, split='test')
+    hamming_loss = []
+    Jaccard_index = []
+    accuracy = [[] for _ in range(28)]
+    for i_batch, sample_batch in enumerate(loader):
+        outputs = __forward_pass(model, sample_batch[0])
+        batch_acc = __count_acc(outputs, sample_batch[1])
+        for i in range(28):
+            accraucy[i].append(batch_acc[i])
+        hamming_loss.append(__count_hamming_loss(outputs, sample_batch[1]))
+        Jaccard_index.append(__cout_Jaccard_index(outputs, sample_batch[1]))
+    # calculate average accuracy for each class
+    for i in range(28):
+        accuracy[i] = np.mean(accuracy[i])
+    metrix = {
+        'hamming': np.mean(hamming_loss),
+        'jaccard': np.mean(Jaccard_index),
+        'accuracy': accuracy,
+    }
+    return metrix
